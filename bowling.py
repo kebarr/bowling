5d6b8818c4a4308c8beaf725eff968465b030fd9
kebarr <katarzinia@gmail.com>
2014-02-08 23:02:03 +0000

    passes final test

--- bowling.py
+++ bowling.py
@@ -5,78 +5,121 @@ class Game:
         self.scores = [0 for i in range(players)] # is this actually used?
         # also need to store whether last roll was a spare or a strike
         self._spare = [False for i in range(players)]
-        self._strike = [False for i in range(players)]
+        self._strike = [[False,0] for i in range(players)] # second number stores number of previous strikes
         # [1st, 2nd, bonus]
         self.frames = [[[None, None, None] for j in range(10)] for i in range(players)]
         self.running_total = [0 for i in range(players)]
         self.player = 0
+        # turn is which of the 1 to 10 goes were on
+        # not which or first or second roll
         self.turn = 0
-        
-    # can move a lot of this functionality into score but couldn't show a running total that way
-    # or, not really even sure why we need a score function, keep tabs of it here
-    # this needs to know whether its the first or second roll, and which go were on
-    # not really sure what a function which only takes pins would do. 
-    # can store which player we're on as a variable and keep tabs of it in here. would need to do the same for roll
+        self.can_roll = [True for i in range(players)]
+        self.last_turn_third_roll = [False for i in range(players)]
+
     def roll(self, pins):
+        if pins > 10:
+            return "cannot hit more than 10 pins"
+        # special case for last roll
+        if self.last_turn_third_roll[self.player]: # can only be this on last roll
+            self.frames[self.player][9][2] = pins
+            self.score_strike()
+            #self.running_total[self.player] += 20 + pins
+            print "last roll"
+        else:
+            self.standard_roll(pins)
+            print "standard roll"
+        return self.running_total
+
+    def standard_roll(self, pins):
         player = self.player
         turn = self.turn
-        next_player = None
         if self.frames[player][turn][0] is None:
-            # on first roll, 
-            next_player = self.first_roll(pins, player, turn)
+            # on first roll, so roll, then score if we had a spare
+            self.first_roll(pins, player)
             if self._spare[player] == True:
                 self.score_spare(pins, player, turn)
         else:
-            if self._strike[player] == True:
-                self.score_strike(pins, player, turn)
-            total = self.second_roll(pins, player, turn)
+            # and whether we can roll, i.e. not a strike this turn.
+            # update turn in second_roll if not a strike.
+            if self.can_roll[player]:
+                self.second_roll(pins, player, turn)            
             self.increment_player()
-            self.turn = 0
-        # increment turn if its not a strike, and not the second roll, so total has been defined
-        elif total:
-            self.turn = 1
-        print self.running_total
-        return self.running_total
+
     
     def increment_player(self):
+        self.can_roll[self.player] = True # this wont make all players able to- 
         if self.player < self.players -1:
             self.player += 1
         elif self.player == self.players -1:
             # we're onto the next round
             self.player = 0
+            if self.turn < 9:
+                self.turn += 1
+                print "turn"
         else:
             print "Exceeded number of players!!"
             
-    def first_roll(self, pins, player, turn):
-        self.frames[player][turn][0] = pins
+    def first_roll(self, pins, player):
+        turn = self.turn
+        self.frames[player][self.turn][0] = pins
         if pins == 10:
-                self._strike[player] = True
-        return self._strike[player]
+                print "strike"
+                if self.players > 1:
+                    if turn != 9:
+                        self.can_roll[player] = False
+                elif turn != 9:
+                    self.turn += 1 # only 1 player so increment turn on strike
+                self._strike[player][0]= True
+                self._strike[player][1] += 1
     
     def second_roll(self, pins, player, turn):
+        print self.running_total, "second roll"
         self.frames[player][turn][1] = pins
-    # if we make the second roll, its definitely not a strike!
-        self._strike[player] == False
+        # if we make the second roll, its definitely not a strike!
+        #self._strike[player] == False
         if self.frames[player][turn][0] + self.frames[player][turn][1] == 10:
-            self._spare[player] == True
+            self._spare[player] = True
         # if its neither a strike nor a spare, we can update the running total
         else:
-            self.running_total[player] += self.frames[player][turn][0] + self.frames[player][turn][1]
-        return self.running_total
-    
-    def score_strike(self, pins, player, turn):
-        self.frames[player][turn-1][2] += pins
-        self.running_total[player]+= self.frames[player][turn-1][0] + self.frames[player][turn-1][1] + self.frames[player][turn-1][2]
+            if turn < 9 or pins < 10:
+                #if turn != 9: don't want to increment running total if last turn and 10 pins
+                self.running_total[player] += self.frames[player][turn][0] + self.frames[player][turn][1]
+                # call score strike regardless of wheher its a strike or not, and deal with that there
+                self.score_strike()
+        #print self.running_total, "second roll 2"
+        if turn == 9 and pins == 10:
+            self.last_turn_third_roll[player] = True
+            print "true"
+
+    def score_strike(self):
+        player = self.player
+        turn = self.turn
+        self._strike[player][0] = False
+        # total each preceeding strike
+        for i in range(self._strike[player][1]):
+            print i
+            # first will always be 2 in a row
+            # test if there was a second throw on this turn
+            if self.frames[player][turn-i][1] is not None:
+                self.frames[player][turn-1 - i][2] = int(self.frames[player][turn - i][0] or 0) + int(self.frames[player][turn -i][1] or 0)
+                self.running_total[player]+= int(self.frames[player][turn-1-i][0] or 0) + int(self.frames[player][turn-1-i][2] or 0)
+                print "score next go"
+            else:
+                self.frames[player][turn-2-i][2] = self.frames[player][turn-1-i][0] + self.frames[player][turn-i][0]
+                self.running_total[player] += self.frames[player][turn-2-i][0] + self.frames[player][turn-2-i][2] # no middle go
+                print "two strikes"
+        self._strike[player][1] = 0
+        print self.running_total, "strike"
 
     def score_spare(self, pins, player, turn):
+        #print "spare"
 # previous frame has score from first roll added
         self.frames[player][turn-1][2] = pins
         # update running total
-        self.running_total[player]+= self.frames[player][turn-1][0] + self.frames[player][turn-1][1] + self.frames[player][turn-1][2]
+        self.running_total[player]+= self.frames[player][turn-1][0] + int(self.frames[player][turn-1][1] or 0) + int(self.frames[player][turn-1][2] or 0)
         # reset to False until score for this turn is calculated
         self._spare[player] = False
 
     def score(self):
-        # exception if we've not finished the game?
         return self.running_total
             
